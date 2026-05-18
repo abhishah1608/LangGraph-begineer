9918c7a99469be9f502f6e16a9f339bed0210eab
Abhi Shah <abhishah1608@gmail.com>
2026-05-18 18:31:13 -0400

    Implementation of created langchain tool and added into it.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents import initialize_agent, AgentType, tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.tools import TavilySearchResults
+from datetime import datetime
 
 load_dotenv()
 
@@ -10,7 +11,13 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.3
 )
 
+@tool
+def get_todayDate(format="%Y-%m-%d %H:%M:%S"):
+    """ Get today's date in the specified format. Default format is 'YYYY-MM-DD HH:MM:SS'.
+    """    
+    return datetime.now().strftime(format)
+
 search = TavilySearchResults(search_depth="basic")
-tools = [search]
+tools = [search, get_todayDate]
 agent = initialize_agent(tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-agent.invoke({"input": "Give me a funny tweek on today's weather in Pierrefonds?"})
+agent.invoke({"input": "When was SpaceX's last launch and how many days ago was that from this instant?"})
