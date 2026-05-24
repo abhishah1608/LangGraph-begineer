ce41a78234d54599b5c40af282c3bf7d159afcc5
Abhi Shah <abhishah1608@gmail.com>
2026-05-24 11:50:42 -0400

    Added one more example

---
+++ basic_react_example.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from datetime import datetime
+from langchain.agents import tool, AgentType, initialize_agent
+from langchain_community.tools import TavilySearchResults
+from langchain_openai import ChatOpenAI
+from zoneinfo import ZoneInfo
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0.3)
+
+@tool
+def get_todayDate(timezone : str):
+    """ Get today's date in the '%Y-%m-%d %H:%M:%S' based on the specified timezone for example America/New_York."""    
+    #print("Timezone received in tool: ", timezone)
+    return datetime.now(ZoneInfo(timezone)).strftime("%Y-%m-%d %H:%M:%S")
+
+tools= [get_todayDate]
+
+agent = initialize_agent(tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+agent.invoke({"input": "What is the current date and time in Quebec City?"})
